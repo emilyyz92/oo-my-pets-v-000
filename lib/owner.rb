0289b33867ad14c5a4824f350270e73ea612a797
Emily Zhao <tong.zhao.92@gmail.com>
2017-11-21 15:51:42 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -35,4 +35,12 @@ class Owner
     @pets[:fishes] << new_fishpet
   end
 
+  def buy_dog (fish_name)
+    new_fishpet = Fish.all.find{|fish|fish.name == fish_name}
+    if new_fishpet == nil
+      new_fishpet = Fish.new(fish_name)
+    end
+    @pets[:fishes] << new_fishpet
+  end
+
 end
